4f0c550a27e2f34290f558f98b6ab85ba0a09512
Toyoda Yoshiteru <6089more@gmail.com>
2021-11-02 10:43:01 +0000

    javascript基礎の復習関数等

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,97 @@
 //= require jquery
 //= require moment
 //= require fullcalendar
+
+
+let  hello = "hello world"
+
+alert(hello);
+
+alert(4+3);
+alert("hello" + "world");
+
+let str1 = "hello"
+let str2 = "world"
+
+alert(str1 + str2);
+
+let orange = 1000;
+let apple = 500;
+
+if(orange < apple){
+  alert("オレンジはりんごより安い");
+}else if(orange == apple){
+  alert("オレンジとりんごは同じ値段");
+}else{
+  alert("オレンジはりんごより高い");
+}
+
+let num = 1;
+let max = 100;
+let count = 1;
+
+while(num < max){
+  num = num * 2;
+  count = count =+ 1
+}
+
+alert("2をかけ続けて" + max + "を超えるまでに" + count + "回でした");
+
+let promptStr = prompt("何か入力をしてください");
+alert(promptStr);
+
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+let js_hand = getJShand();
+
+let judge = winLose(user_hand, js_hand);
+
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+  let hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+
+  return hand_name;
+}
+
+function winLose(user, js){
+  let winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
