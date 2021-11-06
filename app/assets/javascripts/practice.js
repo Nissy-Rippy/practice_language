d9837baa107418a0c147115b9429d0a38aa9bef9
Toyoda Yoshiteru <6089more@gmail.com>
2021-11-06 14:17:22 +0000

    [add]jQeryコード復習

--- app/assets/javascripts/practice.js
+++ app/assets/javascripts/practice.js
@@ -98,3 +98,102 @@ $(function() {
     
   });
 });
+
+
+
+$(function() {
+  $('img').fadeOut();
+  $('p').slideUp();
+});
+
+
+$(function() {
+  $('#title').show();
+  $('#image').fadeIn();
+});
+
+$(function() {
+  $('#hide-text').click(function(){
+    $('#text').slideUp();
+  });
+});
+
+$(function() {
+  // 「#change-css」要素に対するclickイベントを作成してください
+ $('#change-css').click(function(){
+   $('#text').css({
+     'color': 'red',
+     'font-size': '50px'});
+ });
+    
+});
+
+$(function() {
+  $('#change-text').click(function(){
+    $('#text').text('ようこそ、Progateへ');
+  });
+  $('#change-html').click(function(){
+    $('#text').html('<a href="https://prog-8.com/">ようこそ、Progateへ</a>');
+   });
+});
+
+
+$(function() {
+  $('.list-item').click(function(){
+    $(this).css('color','red');
+  });
+});
+
+$(function() {
+  $('#find-method').click(function() {
+    var $wrapper = $('wrapper');
+    
+    $('#wrapper').find('a').css('color','red');
+    
+  });
+  
+  $('#children-method').click(function() {
+    $('#wrapper').children('a').fadeOut();
+  });
+});
+
+// hoverは、（）の引数にfunctionを二つ入れているので()と{}に注意しよう
+
+$(function() {
+  $('#language-wrapper').hover(
+  function(){
+    $('.language-text').fadeIn();
+  },
+  function(){
+    $('.language-text').fadeOut();
+  }
+  );
+});
+
+$(function() {
+  // 「#login-show」要素に対するclickイベントを作成してください
+  $('#login-show').click(function(){
+    $('#login-modal').fadeIn();
+  });
+  
+});
+
+$(function() {
+  $('#hide-btn').click(function(){
+    $('.slide').eq(1).fadeOut();
+  });
+  
+});
+
+
+
+(function() {
+  $('.index-btn').click(function() {
+    $('.active').removeClass('active');
+    
+    var clickedIndex = $('.index-btn').index($(this));
+    
+    $('.slide').eq(clickedIndex).addClass('active');
+    
+  });
+});
