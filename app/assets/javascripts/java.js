8a66a62a451f33d5ca7bce8deb4fb558543b1235
Toyoda Yoshiteru <6089more@gmail.com>
2021-11-08 03:09:32 +0000

    [finish]条件分岐

--- app/assets/javascripts/java.js
+++ app/assets/javascripts/java.js
@@ -26,11 +26,11 @@ class Main {
   }
 }
 
- public static void main(String[] args) 
+ public static void main(String[] args)
     int number;
-    
+
     number = 3;
-    
+
     System.out.println(number);
 
     String name;
@@ -41,17 +41,17 @@ class Main {
 class Main {
   public static void main(String[] args) {
     int number1 = 3;
-    
+
     // int型の変数number2を定義し、7を代入してください
     int number2 = 7;
     System.out.println(number1 * number2);
-    
+
     String text = "プログラミングを勉強しよう";
-    
+
     System.out.println("Progateで" + text);
-  
-  
-  
+
+
+
     class Main {
     public static void main(String[] args) {
     int number = 11;
@@ -61,84 +61,84 @@ class Main {
     number = 9;
     System.out.println(number);
     text = "java";
-    
+
     System.out.println(text);
-    
+
     class Main {
   public static void main(String[] args) {
     int number = 3;
     System.out.println(number);
-    
+
     number = number + 3;
-    
+
     System.out.println(number);
-      
+
   }
 }
 
 class Main {
   public static void main(String[] args) {
     int number = 8;
-    
+
     number *= 7
     System.out.println(number);
-    
+
     number ++;
-    
+
     System.out.println(number);
-    
-    
+
+
     class Main {
   public static void main(String[] args) {
     int length = 6;
     int height = 8;
-    
+
     int rectangleArea = length * height;
     System.out.println(rectangleArea);
-    
+
     int triangleArea = length * height / 2;
 
     System.out.println(triangleArea);
-    
+
   }
 }
 
 class Main {
   public static void main(String[] args) {
- 
+
     double number1 = 8.5;
-    
+
     double number2 = 3.4;
         System.out.println(number1 + number2);
         System.out.println(number1 - number2);
-    
+
   }
-  
-  
+
+
   class Main {
   public static void main(String[] args) {
     int month = 12;
     int date = 31;
-    
+
     System.out.println(month + "月" + date + "日");
-    
+
     System.out.println(7 / 2);
-    
+
     System.out.println(7.0 / 2.0);
-    
+
     System.out.println(7 / 2.0);
-    
+
   }
-  
-  
+
+
   class Main {
   public static void main(String[] args) {
     int number1 = 7;
     int number2 = 2;
     System.out.println(number1 / number2);
-    
+
     System.out.println((double)number1 / number2);
-    
+
   }
 }
 
@@ -146,10 +146,10 @@ class Main {
   public static void main(String[] args) {
     String name = "武藤敬司";
         int age = 33;
-    
+
     double height = 1.2;
     double weight = 20.9;
-    
+
     double bmi = weight / height / height;
 
     System.out.println("名前は" + name + "です");
@@ -159,3 +159,105 @@ class Main {
     System.out.println("BMIは" + bmi +"です");
     }
   }
+
+  class Main {
+  public static void main(String[] args) {
+    System.out.println(true);
+
+    System.out.println(false);
+
+    System.out.println(12/4 == 3);
+
+    System.out.println(12/4 != 3);
+
+    boolean bool = 3 * 9 == 27;
+    System.out.println(bool);
+
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println(4 + 2 < 6);
+
+    System.out.println(4 + 2 <= 6);
+
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    if (true) {
+      System.out.println("条件式がtrueのため、出力されます");
+    }
+
+    if (false) {
+      System.out.println("条件式がfalseのため、出力されません");
+    }
+
+    int x = 5;
+    if( x > 2){
+      System.out.println("xは2より大きい");
+    }
+
+    if(x > 8){
+      System.out.println("xは8より大きい");
+    }
+
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    int number = 12;
+
+    if (number < 10) {
+      System.out.println("10より小さい");
+    }else if(20 > number){
+      System.out.println("10以上、20より小さい");
+    }else{
+      System.out.println("20以上");
+    }
+
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    int number = 12;
+
+    switch(number % 3) {
+    case 0:
+      System.out.println("3で割り切れます");
+      break;
+    case 1:
+     System.out.println("3で割ると1余ります");
+     break;
+    case 2:
+      System.out.println("3で割ると2余ります");
+    break;
+    }
+  }
+}
+
+
+
+class Main {
+  public static void main(String[] args) {
+    int number = 12;
+
+    // switch文を用いて、numberを3で割った余りで条件分岐をしてください
+    switch(number % 3){
+    case 0:
+      System.out.println("3で割り切れます");
+      break;
+    case 1:
+     System.out.println("3で割ると1余ります");
+     break;
+    case 2:
+      System.out.println("3で割ると2余ります");
+      break;
+
+    }
+  }
+}
