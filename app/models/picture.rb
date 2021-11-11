8d89806a38b450b6acf9458f6ea423a750922f01
Toyoda Yoshiteru <6089more@gmail.com>
2021-11-11 15:03:41 +0000

    validatesの復習

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,12 @@
 class Picture < ApplicationRecord
+  #いろいろなvalidatesのコードの練習
+validates :email, format: { with: /\A[a-zA-Z]+\z/}
+validates :number, inclusion: { in: (1..50)}
+validates :name, length: { maximum: 20 }
+validates :name, length: { minimum: 5 }, allow_nil: true
+validates :name, uniqueness: true
+validates :name, length: { minimum: 5 }, allow_blank: true
+validates :email, presence: true, on: :create
+validates :email, format:{ with: /\A[a-zA-Z]+\z/,
+    message: "英文字のみが使用できます" }
 end
